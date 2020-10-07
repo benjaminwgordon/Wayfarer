d168ced9e759ce18757b72181adc5759ed40d99d
Ben Gordon <Bengordon1995@gmail.com>
2020-10-07 16:27:38 -0500

    models confirmed by dalton

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    current_city = models.CharField(max_length=100)
+    join_date = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
+class City(models.Model):
+    image = models.URLField(max_length=200)
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    cities = models.ForeignKey(City, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=1500)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = [-'created_at']
