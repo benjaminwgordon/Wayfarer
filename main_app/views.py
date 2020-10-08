e4716812fb92409304e505b92bc8e4bc78356061
KEYAHNAB <KEYAHNA_BLANFORD@ICLOUD.COM>
2020-10-08 11:18:16 -0400

    home view, semantic ui integrated. home splash page almost complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Profile, City, Post
 
-# Create your views here.
+# Create your views here
+
+
+#  Home view
+
+def home(request):
+    return render(request, 'home.html')
