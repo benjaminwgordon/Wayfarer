97cc8868c176e39ad886f5a94379520b7486a2af
dwannamaker <wannamaker.dylan@gmail.com>
2020-10-09 12:25:30 -0700

    commiting to allow pull

--- main_app/models.py
+++ main_app/models.py
@@ -41,6 +41,6 @@ class Post(models.Model):
     def __str__(self):
         return str(self.author) + self.title
 
-# sorts table newesst first
+# sorts table newest first
     class Meta:
         ordering = ['created_at']
