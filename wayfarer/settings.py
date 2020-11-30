63c7d5e921571ac8b5a837cb2a7b7dad85a1c181
Ben Gordon <Bengordon1995@gmail.com>
2020-11-30 14:19:08 -0600

    added base url configs

--- wayfarer/settings.py
+++ wayfarer/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
